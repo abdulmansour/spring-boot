8b3cb2463d649db306bba63f9121d4b959279c3c
abdulmansour <molate1591@gmail.com>
2020-06-12 14:43:57 -0400

    rest endpoint test '/'

---
+++ src/main/java/com/abdulmansour/springboot/demo/rest/RESTController.java
@@ -0,0 +1,15 @@
+package com.abdulmansour.springboot.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class RESTController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello world! Time on server is " + LocalDateTime.now();
+    }
+}
