e44fe2233d36a019f5c55b0aeeb81583d22e0284
abdulmansour <molate1591@gmail.com>
2020-06-12 14:13:58 -0400

    first commit - spring boot initializr

---
+++ src/main/java/com/abdulmansour/springboot/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.abdulmansour.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
