aae0ae685d9285604e2040c946ffd9853652a7e2
abdulmansour <molate1591@gmail.com>
2020-06-13 18:58:02 -0400

    basic properties configurations

--- src/main/java/com/abdulmansour/springboot/demo/rest/RESTController.java
+++ src/main/java/com/abdulmansour/springboot/demo/rest/RESTController.java
@@ -1,5 +1,7 @@
 package com.abdulmansour.springboot.demo.rest;
 
+import com.abdulmansour.springboot.demo.entity.Developer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +10,21 @@ import java.time.LocalDateTime;
 @RestController
 public class RESTController {
 
+    @Value("${developer.firstName}")
+    private String firstName;
+
+    @Value("${developer.lastName}")
+    private String lastName;
+
     @GetMapping("/")
     public String index() {
         return "Hello world! Time on server is " + LocalDateTime.now();
     }
 
-    @GetMapping("/marathon")
-    public String marathon() {
-        return "Run a 5k marathon!";
+    @GetMapping("/dev")
+    public Developer dev() {
+        Developer developer = new Developer(firstName, lastName);
+        return developer;
     }
 
 }
